fd8a3990ceac287aab5ac558cdbc814d4774b4c1
AlbusSeverus <ericsonhnc@gmail.com>
2023-05-26 14:36:36 -0700

    debug time

--- config/connection.js
+++ config/connection.js
@@ -9,13 +9,13 @@ if (process.env.JAWSDB_URL) {
     }
     // Otherwise, use the local database
     else {
-        sequelize = new Sequelize(
+      const sequelize = new Sequelize(
             process.env.DB_NAME,
             process.env.DB_USER,
-            process.env.DB_PASSWORD,
+            process.env.DB_PW,
             {
-                host: 'localhost',
-                dialect: 'mysql',
+                host: process.env.DB_HOST,
+                dialect: process.env.DB_DIALECT,
                 port: 3306
             }
         );
