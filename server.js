d015091f2d705246563a81eeb0ca356a7f3c6fb1
AlbusSeverus <ericsonhnc@gmail.com>
2023-06-06 15:57:35 -0700

    debugging

--- server.js
+++ server.js
@@ -1,41 +1,35 @@
-const express = require('express');
-const expbs = require('express-handlebars');
-const allRoutes = require('./controllers');
-const session = require('express-session');
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const express = require("express");
+const exphbs = require("express-handlebars");
+const allRoutes = require("./controllers");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const {User,Blog,Comment} = require('./models');
+const { User, Blog, Comment } = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const sess = {
   secret: process.env.DB_SESSION_SECRET,
   cookie: {
-    // Stored in milliseconds (86400 === 1 day)
-    maxAge: 86400,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-    path: '/',
-    sameSite: 'lax',
+    // half an hour
+    maxAge: 0.5 * 60 * 60 * 1000
   },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize,
-  }),
+    db: sequelize
+  })
 };
 app.use(session(sess));
 
 app.use(express.static('public'));
 
-const hbs = expbs.create({});
-
+const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
